989ecef38f9eccce495852dd047c65ff39bf0aa9
Priyal Bansal <98967908+Priyal1610@users.noreply.github.com>
2023-08-10 13:26:40 +0530

    Update LoginForm.aspx.cs

--- LoginForm.aspx.cs
+++ LoginForm.aspx.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -13,5 +14,36 @@ namespace WebApplication17
         {
 
         }
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            String userId = TextBox1.Text;
+            String password = TextBox2.Text;
+
+            string connString = "host = localhost; database = friend2s_db; username=root; password =priyalsql@1610";
+            MySqlConnection conn = new MySqlConnection(connString);
+
+            //open the connection
+            conn.Open();
+
+            //pass the query
+            string query = $"select * from Users where user_id = '{userId}' and passwordIn = '{password}';";
+
+
+            //create mySql command object to execute command
+            MySqlCommand cmd = new MySqlCommand(query, conn);
+
+            MySqlDataReader reader = cmd.ExecuteReader();
+
+            if (reader.HasRows)
+            {
+                Response.Write("<script> alert('Login Successful!!');</script>");
+            }
+            else
+            {
+                Response.Write("<script> alert('Invalid User Id or Password');</script>");
+            }
+
+            //close connection
+            conn.Close();
     }
 }
